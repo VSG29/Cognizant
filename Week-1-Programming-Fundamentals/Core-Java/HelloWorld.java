989cafb999e0d9924dec93f1a92a1e3c4b853f15
VSG29 <varunsh2005@gmail.com>
2026-07-06 09:41:55 +0530

    Deleted

--- Week-1-Programming-Fundamentals/Core-Java/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, Cognizant!");
-        System.out.println("Welcome to Core Java.");
-    }
-}
