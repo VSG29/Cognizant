1410d71b53d305cd64e4ea2f4ad0a18a6e1d348d
VSG29 <varunsh2005@gmail.com>
2026-07-25 14:29:18 +0530

    Completed Exercise 1: Spring Data JPA Quick Example

---
+++ WEEK_3/LibraryManagement/orm-learn/src/main/java/com/cognizant/ormlearn/repository/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.cognizant.ormlearn.repository;
+
+import com.cognizant.ormlearn.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CountryRepository extends JpaRepository<Country, String> {
+
+}
