c8b28bfe87560b8bf36cd69695b1788c207a7069
VSG29 <varunsh2005@gmail.com>
2026-07-27 13:28:59 +0530

    Week 5 - React Exercise 2

---
+++ WEEK_5/React/studentapp/src/Components/Contact.js
@@ -0,0 +1,9 @@
+function Contact() {
+    return (
+        <div>
+            <h1>Welcome to the Contact page of Student Management Portal</h1>
+        </div>
+    );
+}
+
+export default Contact;
